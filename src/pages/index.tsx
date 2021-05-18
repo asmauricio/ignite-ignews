11d32a7b45cfa105aa7d41e54d797ffb5f8de954
Adriano Mauricio <adrianosm@msn.com>
2021-05-18 18:49:59 -0300

    Iniciando componente Header

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/Head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <h1>
+        Hello <span>Adriano...</span>
+      </h1>      
+    </>
+  )
+}
