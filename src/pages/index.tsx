2f6c5a86b7aeb9e21d875f4f7d959efe24fec98d
Adriano Mauricio <adrianosm@msn.com>
2021-05-19 20:27:30 -0300

    Iniciando Stripe e NextAuth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,61 @@
 import Head from 'next/Head'
+import { GetStaticProps } from 'next'
+import { SubscribeButton } from '../components/SubscribeButton'
 
-export default function Home() {
+import styles from './home.module.scss'
+import { stripe } from '../services/stripe'
+
+// Formas de acesso das Páginas
+// Client Side rendering
+// server side rendering
+// Static Site Generation
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
         <title>Inicio | ig.news</title>
       </Head>
-      <h1>
-        Hello <span>Adriano...</span>
-      </h1>      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+        <img src="/images/avatar.svg" alt="Garota codando" />
+      </main>      
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IswyRFLGfUvfvfcNGh40gJS', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 horas
+  }
 }
