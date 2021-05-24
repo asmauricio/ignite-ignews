169e9aad6d5af10f7ee1976e5a75f5e2b97bae45
Adriano Mauricio <adrianosm@msn.com>
2021-05-24 17:29:43 -0300

    Finalizada integração com Stripe e FaunaDB

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -38,7 +38,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
           q.Ref(q.Collection('users'), user.ref.id),
           { 
             data: { 
-              stripe_custom_id: stripeCustomer.id,
+              stripe_customer_id: stripeCustomer.id,
             }
           }  
         )
